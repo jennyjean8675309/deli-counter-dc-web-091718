0da5c6a8a0989aadd3784b306ee47d42c96adc32
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-30 15:31:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,12 @@ katz_deli = []
 def line(deli_line)
   if deli_line.size == 0
     puts "The line is currently empty."
+  else
+    line_order = []
+    deli_line.each_with_index do |name, index|
+      line_order.push("#{index + 1}. #{name}")
+    end
+    puts "The line is currently: "
+      
   
 end
